b5897cc19aeba862fc64e892d913aca86ffa66ef
Hung Tran <tran.huy.hung.20.01@gmail.com>
2024-11-27 00:00:45 +0700

    #

---
+++ src/Components/layout/SideBar/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+SideBar.propTypes = {
+    
+};
+
+function SideBar(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default SideBar;
