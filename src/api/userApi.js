2dc82c87332b5a99244366fdcd31fd6312a2c66f
Trần Huy Hùng <126651249+Hungtran97@users.noreply.github.com>
2024-11-28 10:46:28 +0000

    setup apiClient

---
+++ src/api/userApi.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import axiosClient from './axiosClient';
+
+const userApi = {
+  getAll(params) {
+    url = '/users';
+    return axiosClient.get(url, { params });
+  },
+  get(id) {
+    url = `/user/${id}`;
+    return axiosClient.get(url);
+  },
+  create(data) {
+    url = `/user`;
+    return axiosClient.post(url, data);
+  },
+  update(data) {
+    url = `/user/${data.id}`;
+    return axiosClient.put(url, data);
+  },
+  remove(id) {
+    url = `/user/${id}`;
+    return axiosClient.delete(url);
+  },
+};
+export default userApi;
